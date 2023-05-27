9344a9461bfc28a7794762665bb9c7722d27f01d
daryna.fesenko <darina071602@gmail.com>
2023-05-27 23:27:13 +0200

    ex7

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("Число " + number + " является четным");
+}
+else
+{
+    System.Console.WriteLine("Число " + number + " не является нечетным");
+}
